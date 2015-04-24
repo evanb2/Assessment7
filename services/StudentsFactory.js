3c617c89573b8552b850ef5285b26ea9be1fad2f
Evan Butler <evanb2@mac.com>
2015-04-24 12:00:31 -0700

    get students to display, and add signIn list

--- services/StudentsFactory.js
+++ services/StudentsFactory.js
@@ -1,10 +1,8 @@
 rollCall.factory('StudentsFactory', function StudentsFactory() {
   var factory = {};
   factory.students = [];
-
   factory.addStudent = function(name) {
     this.students.push({ name: name, signIn: false });
-    console.log(this.students);
   };
   return factory;
 });
