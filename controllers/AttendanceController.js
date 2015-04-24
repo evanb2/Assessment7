bf21fbd170c3ee678c6837bd342bfbbbff25cfb6
Evan Butler <evanb2@mac.com>
2015-04-24 12:24:10 -0700

    add removeSignIn function and button

--- controllers/AttendanceController.js
+++ controllers/AttendanceController.js
@@ -3,4 +3,8 @@ rollCall.controller('AttendanceCtrl', function AttendanceCtrl($scope, StudentsFa
   $scope.addSignIn = function(student) {
     student.signIn = true;
   };
+
+  $scope.removeSignIn = function(student) {
+    student.signIn = false;
+  };
 });
