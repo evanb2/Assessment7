0df231de69c4643cde1410d298459db72c3cb9aa
Evan Butler <evanb2@mac.com>
2015-04-24 11:20:22 -0700

    edit name property in StudentsFactory

--- services/StudentsFactory.js
+++ services/StudentsFactory.js
@@ -1,8 +1,10 @@
 rollCall.factory('StudentsFactory', function StudentsFactory() {
   var factory = {};
   factory.students = [];
+
   factory.addStudent = function(name) {
-    this.students.push({ name: factory.studentName, signIn: false });
+    this.students.push({ name: name, signIn: false });
+    console.log(this.students);
   };
   return factory;
 });
